77825febfaa5d818c5f57a27b241bf32bbf02a7b
hbc <bcxxxxxx@gmail.com>
2016-02-01 23:02:05 +0800

    chore: init.

---
+++ src/cmd/read.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	"gopkg.in/telegram-bot-api.v1"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+}
