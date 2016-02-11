34ca405bbcee9893121f338945541d8252e4863e
hbc <bcxxxxxx@gmail.com>
2016-02-11 22:08:28 +0800

    feat: implement GetInPeriod & Forget

--- src/brain/brain.go
+++ src/brain/brain.go
@@ -2,45 +2,62 @@
 package brain
 
 import (
+	"sort"
 	"sync"
 	"time"
+
+	"github.com/SaidinWoT/timespan"
 )
 
 type Brain interface {
 	Remember(at time.Time, key, thing string) error
 	Get(key string) (string, bool)
+	GetInPeriod(period timespan.Span) []string
+	Forget(key string) error
 }
 
 type thing struct {
 	At      time.Time
+	Key     string
 	Payload string
 }
 
+type things []thing
+
+func (t things) Len() int           { return len(t) }
+func (t things) Swap(i, j int)      { t[i], t[j] = t[j], t[i] }
+func (t things) Less(i, j int) bool { return t[i].At.Before(t[j].At) }
+
 type brain struct {
-	things     map[string]thing
-	thingsLock *sync.RWMutex
+	l *sync.RWMutex
+
+	things      map[string]thing
+	periodIndex []thing
 }
 
 func NewBrain() *brain {
 	return &brain{
-		things:     make(map[string]thing),
-		thingsLock: &sync.RWMutex{},
+		things:      make(map[string]thing),
+		periodIndex: []thing{},
+		l:           &sync.RWMutex{},
 	}
 }
 
 func (b *brain) Remember(at time.Time, key, payload string) error {
-	b.thingsLock.Lock()
-	defer b.thingsLock.Unlock()
+	b.l.Lock()
+	defer b.l.Unlock()
 
-	t := thing{at, payload}
+	t := thing{at, key, payload}
 	b.things[key] = t
+	b.periodIndex = append(b.periodIndex, t)
+	sort.Sort(things(b.periodIndex))
 
 	return nil
 }
 
 func (b brain) Get(key string) (string, bool) {
-	b.thingsLock.RLock()
-	defer b.thingsLock.RUnlock()
+	b.l.RLock()
+	defer b.l.RUnlock()
 
 	thing, present := b.things[key]
 	if present {
@@ -48,4 +65,48 @@ func (b brain) Get(key string) (string, bool) {
 	}
 
 	return "", present
+}
+
+func (b brain) GetInPeriod(period timespan.Span) []string {
+	b.l.RLock()
+	defer b.l.RUnlock()
+
+	i := sort.Search(
+		len(b.periodIndex),
+		func(i int) bool {
+			return period.ContainsTime(b.periodIndex[i].At)
+		},
+	)
+
+	rv := []string{}
+	for ; i < len(b.periodIndex); i++ {
+		if !period.ContainsTime(b.periodIndex[i].At) {
+			break
+		}
+		rv = append(rv, b.periodIndex[i].Payload)
+	}
+
+	return rv
+}
+
+func (b *brain) Forget(key string) error {
+	b.l.Lock()
+	defer b.l.Unlock()
+
+	if _, present := b.things[key]; !present {
+		return nil
+	}
+
+	delete(b.things, key)
+	for i := 0; i < len(b.periodIndex); i++ {
+		if b.periodIndex[i].Key == key {
+			b.periodIndex = append(
+				b.periodIndex[:i],
+				b.periodIndex[i+1:]...,
+			)
+			break
+		}
+	}
+
+	return nil
 }
--- src/brain/brain_test.go
+++ src/brain/brain_test.go
@@ -3,7 +3,17 @@ package brain
 import (
 	"testing"
 	"time"
+
+	"github.com/SaidinWoT/timespan"
 )
+
+func parseTime(layout, str string) time.Time {
+	rv, err := time.Parse(layout, str)
+	if err != nil {
+		panic(err)
+	}
+	return rv
+}
 
 func TestNewBrain(t *testing.T) {
 	brain := NewBrain()
@@ -57,5 +67,118 @@ func TestRememberAndGet(t *testing.T) {
 
 	if thing != newThing {
 		t.Errorf("expected %s, got %s", newThing, thing)
+	}
+}
+
+func TestGetInPeriod(t *testing.T) {
+	var (
+		err    error
+		span   timespan.Span
+		things []string
+	)
+
+	brain := NewBrain()
+	err = brain.Remember(parseTime("2006-01-02", "2015-01-01"), "t1", "t1")
+	if err != nil {
+		t.Error(err)
+	}
+	err = brain.Remember(parseTime("2006-01-02", "2015-02-01"), "t2", "t2")
+	if err != nil {
+		t.Error(err)
+	}
+	err = brain.Remember(parseTime("2006-01-02", "2015-02-01"), "t3", "t3")
+	if err != nil {
+		t.Error(err)
+	}
+	err = brain.Remember(parseTime("2006-01-02", "2015-02-02"), "t4", "t4")
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Cover everythings
+	span = timespan.New(
+		parseTime("2006-01-02", "2015-01-01"),
+		time.Duration(365*24)*time.Hour,
+	)
+	things = brain.GetInPeriod(span)
+	if len(things) != 4 {
+		t.Errorf("expected 4 things, got: %v", things)
+	}
+
+	// Cover nothings (from left side)
+	span = timespan.New(
+		parseTime("2006-01-02", "2014-01-01"),
+		time.Duration(365*24)*time.Hour,
+	)
+	things = brain.GetInPeriod(span)
+	if len(things) != 0 {
+		t.Errorf("expected 0 things, got: %v", things)
+	}
+
+	// Cover nothings (from right side)
+	span = timespan.New(
+		parseTime("2006-01-02", "2016-01-01"),
+		time.Duration(365*24)*time.Hour,
+	)
+	things = brain.GetInPeriod(span)
+	if len(things) != 0 {
+		t.Errorf("expected 0 things, got: %v", things)
+	}
+
+	// Partial cover
+	span = timespan.New(
+		parseTime("2006-01-02", "2015-02-01"),
+		time.Duration(1)*time.Hour,
+	)
+	things = brain.GetInPeriod(span)
+	if len(things) != 2 {
+		t.Errorf("expected 2 things, got: %v", things)
+	}
+	if things[0] != "t2" {
+		t.Errorf("expected 't2', got: %s", things[0])
+	}
+	if things[1] != "t3" {
+		t.Errorf("expected 't3', got: %s", things[1])
+	}
+}
+
+func TestForget(t *testing.T) {
+	var (
+		err             error
+		retrievedThing  string
+		retrievedThings []string
+		present         bool
+	)
+
+	at := time.Now()
+	key := "foo"
+	thing := "bar"
+	brain := NewBrain()
+	err = brain.Remember(at, key, thing)
+	if err != nil {
+		t.Error(err)
+	}
+
+	retrievedThing, present = brain.Get(key)
+	if !present {
+		t.Errorf("expected true")
+	}
+	if retrievedThing != thing {
+		t.Errorf("expected %s, got: %s", thing, retrievedThing)
+	}
+
+	err = brain.Forget(key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	retrievedThing, present = brain.Get(key)
+	if present {
+		t.Errorf("expected false")
+	}
+	span := timespan.New(at, time.Duration(1)*time.Hour)
+	retrievedThings = brain.GetInPeriod(span)
+	if len(retrievedThings) != 0 {
+		t.Errorf("expected 0 things, got: %v", retrievedThings)
 	}
 }
